bf7dd4c54d0fa5856e757eb3403e98bedea7ccaf
rachids <livecoding@rachids.ca>
2021-08-06 18:00:31 -0400

    First commit

---
+++ config/dashboard.php
@@ -0,0 +1,44 @@
+<?php
+
+return [
+    /*
+     * The dashboard supports these themes:
+     *
+     * - light: always use light mode
+     * - dark: always use dark mode
+     * - device: follow the OS preference for determining light or dark mode
+     * - auto: use light mode when the sun is up, dark mode when the sun is down
+     */
+    'theme' => 'device',
+
+    /*
+     * When the dashboard uses the `auto` theme, these coordinates will be used
+     * to determine whether the sun is up or down.
+     */
+    'auto_theme_location' => [
+        'lat' => 46.829853,
+        'lng' => -71.254028,
+    ],
+
+    /*
+     * These scripts will be loaded when the dashboard is displayed.
+     */
+    'scripts' => [
+        'alpinejs' => 'https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js',
+    ],
+
+    /*
+     * These stylesheets will be loaded when the dashboard is displayed.
+     */
+    'stylesheets' => [
+        'inter' => 'https://rsms.me/inter/inter.css'
+    ],
+
+    'tiles' => [
+        'weather_forecast' => [
+            'open_weather_map_key' => env('OPEN_WEATHER_MAP_KEY'),
+            'open_weather_map_city' => 'Québec',
+            'units' => 'metric', // 'metric' or 'imperial' (metric is default)
+        ],
+    ],
+];
