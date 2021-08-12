3e1117e2df924f63a920b55241eeb6574b8a2054
rachids <livecoding@rachids.ca>
2021-08-12 16:48:39 -0400

    Change json column to longtext

--- database/migrations/2021_08_06_212114_create_dashboard_tiles_table.php
+++ database/migrations/2021_08_06_212114_create_dashboard_tiles_table.php
@@ -11,7 +11,7 @@ class CreateDashboardTilesTable extends Migration
         Schema::create('dashboard_tiles', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->json('data')->nullable();
+            $table->longText('data')->nullable();
             $table->timestamps();
         });
     }
