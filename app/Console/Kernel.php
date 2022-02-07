8b7a32aae532b2127b47ab5dc1bd49aa1b248bd6
rachids <livecoding@rachids.ca>
2022-02-06 20:53:40 -0500

    Add next holiday command to scheduler

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\FetchNextHolidayCommand;
 use App\Console\Commands\FetchTrainingDataCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
@@ -28,6 +29,7 @@ class Kernel extends ConsoleKernel
     {
         $schedule->command(FetchDataFromApiCommand::class)->daily();
         $schedule->command(FetchTrainingDataCommand::class)->daily();
+        $schedule->command(FetchNextHolidayCommand::class)->daily();
     }
 
     /**
